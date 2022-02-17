e1f3e7eb08e3f32aa13a82a058349441c4205fba
Luísa Vilas Boas dos Santos <luisa13santos@outlook.com>
2022-02-16 23:43:30 -0300

    Ate o requisito 9.Repaginando tuuuudoooo, agora vai certinho

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// criando uma função de adicão para evitar repetições
 function addHTML(parent, child) {
   parent.appendChild(child);
 }
+
+// requisito 4: criando uma lista ordenada e adicionando a página HTML
 const list = document.createElement('ol');
 list.setAttribute('id', 'lista-tarefas');
 
@@ -8,42 +11,54 @@ const createTaskBoard = document.getElementById('cabecalho');
 addHTML(createTaskBoard, list);
 
 // Vai ser mais fácil criar o botão no HTML
+
 // const addTaskButton = document.createElement('button');
 // addTaskButton.setAttribute('id', 'criar-tarefa');
 // addHTML(createTask, addTaskButton);
 
+// pegando o botão e o input // requisitos 3 e 5 feitos no HTML
 const createTaskButton = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 
+// alterando o value pre definido do input
 input.addEventListener('click', () => {
   input.value = '';
 });
 
+// requisitos 7 e 8: refazendo requisitos pra deixar coerente agora que o eventListener acontece depois da tarefa
+
+function bgColor(event) {
+  const item = event.target;
+  const grayItem = document.querySelector('.gray');
+  if (grayItem !== null) { // demorou meio século pra entender esse negócio do null, pois estava sempre colocando
+    grayItem.classList.remove('gray'); // ainda tenho que entender por que as vezes não funciona
+  }
+  item.classList.add('gray');
+}
+
+// requisito 9: doubleclick riscado
+
+function line(event) {
+  // const item = event.target;
+  if (event.target.className === 'task completed gray'){ 
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+
+// event.target.classList.toggle('completed')
+
+// requisito 5: criar itens da lista através do input
+// --  agora que coloquei todos os chamados aqui tudo faz sentido, VALEU CAIQUE!!
 function addTask() {
   const listItem = document.createElement('li');
   listItem.innerText = input.value;
   listItem.className = 'task';
+  listItem.addEventListener('dblclick', line);
+  listItem.addEventListener('click', bgColor);
   input.value = '';
-  list.appendChild(listItem);
+  addHTML(list, listItem);
 }
 
 createTaskButton.addEventListener('click', addTask);
-
-function addColorTaskItem() {
-  list.addEventListener('click', (event) => {
-    const tarefa = event.target;
-    tarefa.className = 'gray';
-  });
-}
-
-addColorTaskItem();
-
-function removeColorTaskItem() {
-  list.addEventListener('click', (event) => {
-    const grayItem = document.querySelector('.gray');
-    grayItem.classList.remove('gray');
-    event.target.classList.add('gray');
-  });
-}
-
-removeColorTaskItem();
